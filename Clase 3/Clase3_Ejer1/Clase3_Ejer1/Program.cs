c75684b95d2cd0e904fd420dde8a11dd080cba2f
Tairshus <alan_tintaya@hotmail.com>
2021-08-30 22:03:47 -0300

    ejercicio 1 clase 3

---
+++ Clase 3/Clase3_Ejer1/Clase3_Ejer1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Cuentas;
+
+namespace Clase3_Ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Creo que necesito un prestamo";
+            Cuenta c1 = new Cuenta("Charly Walle", 500);
+            Console.WriteLine(c1.Mostrar());
+            if (!(c1.Ingresar(500)))
+                Console.WriteLine("El monto ingresado no es valido");
+
+            Console.WriteLine(c1.Mostrar());
+            if (!(c1.Ingresar(-30)))
+                Console.WriteLine("El monto ingresado no es valido");
+
+            Console.WriteLine(c1.Mostrar());
+            c1.Retirar(50);
+
+            Console.WriteLine(c1.Mostrar());
+        }
+    }
+}
---
+++ Clase 3/Clase3_Ejer1/Cuenta/Cuenta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Cuentas
+{
+    public class Cuenta
+    {
+        private string titular;
+        private decimal cantidad;
+
+        public Cuenta(string titular, decimal cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string GetTitular()
+        {
+            return titular;
+        }
+
+        public decimal GetCantidad()
+        {
+            return cantidad;
+        }
+
+        public string Mostrar()
+        {
+            return $"Titular: {GetTitular()}, Cantidad: {GetCantidad()}"; 
+        }
+
+        public bool Ingresar(decimal cantidadAcreditar)
+        {
+            if(cantidadAcreditar > 0)
+            {
+                cantidad += cantidadAcreditar;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Retirar(decimal cantidadRetirar)
+        {
+            cantidad -= cantidadRetirar;
+        }
+    }
+}
